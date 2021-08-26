d7f60972704d9f1f4500538901aee52f39921ecc
Cansiny0320 <1136843910@qq.com>
2021-08-27 00:08:50 +0800

    feat: 增加信息提示

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const label = document.querySelector('label')
 const image = document.querySelector('img')
 const saveBtn = document.querySelector('button')
 const colorInput = document.querySelector('input[type="color"]')
+const tips = document.querySelector('.tips')
 let canvas = null
 
 input.addEventListener('change', e => {
@@ -10,10 +11,14 @@ input.addEventListener('change', e => {
   const file = input.files[0]
   fileReader.readAsDataURL(file)
   fileReader.onload = async function () {
+    image.src = ''
+    label.style.backgroundColor = '#000'
+    tips.textContent = '正在解析，请稍等...'
     const result = await rgbaster(this.result, {
       ignore: ['rgb(255,255,255)', 'rgb(0,0,0)'],
       scale: 0.6,
     })
+    tips.textContent = ''
     const mainColor = result[0].color
     image.src = this.result
     label.style.backgroundColor = mainColor
